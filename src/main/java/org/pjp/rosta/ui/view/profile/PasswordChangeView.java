a9d8ab4555f23e8cd29bf0bcb0820bd6fe5d3f27
Paul <Paul@mayapan>
2022-06-14 19:51:51 +0100

    password change strength measurement with an indicator

--- src/main/java/org/pjp/rosta/ui/view/profile/PasswordChangeView.java
+++ src/main/java/org/pjp/rosta/ui/view/profile/PasswordChangeView.java
@@ -1,39 +1,56 @@
 package org.pjp.rosta.ui.view.profile;
 
+import java.io.IOException;
+
 import javax.annotation.security.RolesAllowed;
 
 import org.pjp.rosta.model.User;
 import org.pjp.rosta.security.SecurityUtil;
 import org.pjp.rosta.service.UserService;
 import org.pjp.rosta.ui.view.CompactHorizontalLayout;
+import org.pjp.rosta.ui.view.CompactVerticalLayout;
 import org.pjp.rosta.ui.view.MainLayout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.nulabinc.zxcvbn.StandardDictionaries;
+import com.nulabinc.zxcvbn.StandardKeyboards;
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.textfield.Autocomplete;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.data.binder.ValidationResult;
 import com.vaadin.flow.data.binder.ValueContext;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+@CssImport(themeFor = "vaadin-progress-bar", value = "./styles/dynamically-change-progressbar-color.css")
 @RolesAllowed("USER")
 @PageTitle("Change Password")
 @Route(value = "password", layout = MainLayout.class)
 public class PasswordChangeView extends VerticalLayout implements AfterNavigationObserver {
 
     private static final long serialVersionUID = -2024122079440962290L;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PasswordChangeView.class);
 
     public static class PasswordBean {
         private String password;
@@ -56,13 +73,26 @@ public class PasswordChangeView extends VerticalLayout implements AfterNavigatio
         }
     }
 
+    static {
+        try {
+            StandardDictionaries.loadAllDictionaries();
+            StandardKeyboards.loadAllKeyboards();
+        } catch (IOException e) {
+            LOGGER.warn("failed to load Zxcvbn files", e);
+        }
+    }
+
+    private final Zxcvbn zxcvbn = new Zxcvbn();
+
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     private final PasswordField password = new PasswordField("Password");
 
     private final PasswordField confirmPassword = new PasswordField("Confirm Password");
 
-    private Span errorMessageField = new Span();
+    private final ProgressBar strengthBarIndicator = new ProgressBar();
+
+    private final Span errorMessageField = new Span();
 
     private final Binder<PasswordBean> binder = new BeanValidationBinder<>(PasswordBean.class);
 
@@ -81,6 +111,25 @@ public class PasswordChangeView extends VerticalLayout implements AfterNavigatio
     public PasswordChangeView() {
         password.setMaxLength(20);
         confirmPassword.setMaxLength(password.getMaxLength());
+
+        password.setAutocomplete(Autocomplete.NEW_PASSWORD);
+        confirmPassword.setAutocomplete(Autocomplete.NEW_PASSWORD);
+
+        password.addValueChangeListener(l -> {
+            Strength strength = zxcvbn.measure(l.getValue());
+            int score = strength.getScore();
+
+            strengthBarIndicator.setValue(score);
+
+            if (score < 2) {
+                strengthBarIndicator.getStyle().set("--progress-color", "#ff0000");
+            } else if (score > 2) {
+                strengthBarIndicator.getStyle().set("--progress-color", "#00ff00");
+            } else {
+                strengthBarIndicator.getStyle().set("--progress-color", "#0000ff");
+            }
+        });
+        password.setValueChangeMode(ValueChangeMode.EAGER);
 
         binder.bindInstanceFields(this);
 
@@ -129,11 +178,25 @@ public class PasswordChangeView extends VerticalLayout implements AfterNavigatio
             reset.setEnabled(hasChanges);
         });
 
+        VerticalLayout strengthBar = getStrengthBar();
+
         HorizontalLayout controls = new CompactHorizontalLayout(save, reset);
         controls.setJustifyContentMode(JustifyContentMode.CENTER);
         controls.setWidthFull();
 
-        add(formLayout, errorMessageField, controls);
+        add(formLayout, strengthBar, errorMessageField, controls);
+    }
+
+    private VerticalLayout getStrengthBar() {
+        Div strengthBarLabel = new Div();
+        strengthBarLabel.setText("Password strength");
+
+        strengthBarIndicator.setMin(0);
+        strengthBarIndicator.setMax(4);
+
+        VerticalLayout strengthBar = new CompactVerticalLayout(strengthBarLabel, strengthBarIndicator);
+        strengthBar.setWidth("50%");
+        return strengthBar;
     }
 
     @Override
@@ -163,6 +226,11 @@ public class PasswordChangeView extends VerticalLayout implements AfterNavigatio
 
         if (pass1 == null || pass1.length() < 8) {
             return ValidationResult.error("Password should be at least 8 characters long");
+        }
+
+        Strength strength = zxcvbn.measure(pass1);
+        if (strength.getScore() < 2) {
+            return ValidationResult.error("Password is not of sufficient strength: " + strength.getFeedback().getWarning());
         }
 
         if (!enablePasswordValidation) {
