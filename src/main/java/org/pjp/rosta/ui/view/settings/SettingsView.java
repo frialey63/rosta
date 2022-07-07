ee4dcb9bb290af7e4dbe260eddf5e642f1a8287a
Paul <Paul@mayapan>
2022-07-07 21:38:53 +0100

    show notification settings in a RO table

--- src/main/java/org/pjp/rosta/ui/view/settings/SettingsView.java
+++ src/main/java/org/pjp/rosta/ui/view/settings/SettingsView.java
@@ -4,9 +4,11 @@ import javax.annotation.security.PermitAll;
 
 import org.pjp.rosta.ui.view.AbstractView;
 import org.pjp.rosta.ui.view.MainLayout;
+import org.springframework.beans.factory.annotation.Value;
+import org.vaadin.stefan.table.Table;
+import org.vaadin.stefan.table.TableRow;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -19,11 +21,28 @@ public class SettingsView extends AbstractView implements AfterNavigationObserve
 
     private static final long serialVersionUID = -4308583634742400882L;
 
+    @Value("${check.rosta.director.email}")
+    private String checkRostaDirectorEmail;
+
+    @Value("${check.rosta.cron}")
+    private String checkRostaCron;
+
+    private Table table;
+
     public SettingsView() {
         setSpacing(false);
 
-        add(new H2("TODO"));
-        add(new Paragraph("Settings for notifications, etc."));
+        add(new H1("Settings for Notifications"));
+
+        table = new Table();
+        table.getElement().setAttribute("border", "1px solid black");
+        table.setWidthFull();
+
+        TableRow headerRow = table.addRow();
+        headerRow.addHeaderCell().setText("Setting");
+        headerRow.addHeaderCell().setText("Value");
+
+        add(table);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
@@ -33,6 +52,13 @@ public class SettingsView extends AbstractView implements AfterNavigationObserve
 
     @Override
     public void afterNavigation(AfterNavigationEvent event) {
+        TableRow detailsRow = table.addRow();
+        detailsRow.addDataCell().setText("check.rosta.director.email");
+        detailsRow.addDataCell().setText(checkRostaDirectorEmail);
+
+        detailsRow = table.addRow();
+        detailsRow.addDataCell().setText("check.rosta.cron");
+        detailsRow.addDataCell().setText(checkRostaCron);
     }
 
 }
