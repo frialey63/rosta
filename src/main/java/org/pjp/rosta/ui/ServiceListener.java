c24ea804f23d86995a5ff9a72733722062befbe4
Paul <Paul@mayapan>
2022-07-21 09:40:10 +0100

    disable idle-notification because it breaks production build (hang) and include copy of winsw.xml from target

--- src/main/java/org/pjp/rosta/ui/ServiceListener.java
+++ src/main/java/org/pjp/rosta/ui/ServiceListener.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.vaadin.componentfactory.IdleNotification;
+//import com.vaadin.componentfactory.IdleNotification;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 
@@ -16,7 +16,7 @@ public class ServiceListener implements VaadinServiceInitListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VaadinServiceInitListener.class);
 
-    private static final int SECONDS_BEFORE_NOTIFICATION = 60;
+//    private static final int SECONDS_BEFORE_NOTIFICATION = 60;
 
     @Value("${session.max.inactive.interval:60}")
     private int maxInactiveInterval;
@@ -29,29 +29,29 @@ public class ServiceListener implements VaadinServiceInitListener {
             initEvent.getSession().getSession().setMaxInactiveInterval(maxInactiveInterval);
         });
 
-        event.getSource().addUIInitListener(initEvent -> {
-            LOGGER.debug("A new UI  has been initialized! maxInactiveInterval = {}, secondsBeforeNotification = {}", maxInactiveInterval, SECONDS_BEFORE_NOTIFICATION);
-
-            initEvent.getUI().getPage().fetchCurrentURL(url -> {
-                String path = url.getPath();
-
-                if ("/login".equals(path) || "/register".equals(path)) {
-                    // skip
-                } else {
-                    if (maxInactiveInterval > 0) {
-                        IdleNotification idleNotification = new IdleNotification(
-                                "Your session will expire in " + IdleNotification.MessageFormatting.SECS_TO_TIMEOUT + " seconds.",
-                                SECONDS_BEFORE_NOTIFICATION,
-                                maxInactiveInterval,
-                                "login");
-                        idleNotification.addExtendSessionButton("Extend session");
-                        idleNotification.addRedirectButton("Logout now", "login");
-                        idleNotification.setExtendSessionOnOutsideClick(false);
-
-                        initEvent.getUI().add(idleNotification);
-                    }
-                }
-            });
-        });
+//        event.getSource().addUIInitListener(initEvent -> {
+//            LOGGER.debug("A new UI  has been initialized! maxInactiveInterval = {}, secondsBeforeNotification = {}", maxInactiveInterval, SECONDS_BEFORE_NOTIFICATION);
+//
+//            initEvent.getUI().getPage().fetchCurrentURL(url -> {
+//                String path = url.getPath();
+//
+//                if ("/login".equals(path) || "/register".equals(path)) {
+//                    // skip
+//                } else {
+//                    if (maxInactiveInterval > 0) {
+//                        IdleNotification idleNotification = new IdleNotification(
+//                                "Your session will expire in " + IdleNotification.MessageFormatting.SECS_TO_TIMEOUT + " seconds.",
+//                                SECONDS_BEFORE_NOTIFICATION,
+//                                maxInactiveInterval,
+//                                "login");
+//                        idleNotification.addExtendSessionButton("Extend session");
+//                        idleNotification.addRedirectButton("Logout now", "login");
+//                        idleNotification.setExtendSessionOnOutsideClick(false);
+//
+//                        initEvent.getUI().add(idleNotification);
+//                    }
+//                }
+//            });
+//        });
     }
 }
---
+++ winsw/run.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar rosta-0.3.0-SNAPSHOT.jar
